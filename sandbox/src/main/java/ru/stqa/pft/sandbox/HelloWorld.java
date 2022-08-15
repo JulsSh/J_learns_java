b39ba123304827f33bf342f460e399223f36d6d5
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-15 16:20:19 +0200

    1.3 changes to progr

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -4,5 +4,18 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello, World");
+		System.out.println(2+2);
+    System.out.println(2*2);
+    System.out.println(2/2);
+    System.out.println(2-2);
+    System.out.println(1/2);
+    System.out.println(1.0/2);
+    System.out.println(2.0/2);
+    System.out.println("2"+"2");
+    System.out.println("2"+2);
+    System.out.println(2+2*2);
+    System.out.println("2 + 2 = " + (2+2));
+
+
   }
 }
