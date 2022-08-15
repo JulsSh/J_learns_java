7d7f58bd6407633992671b3ed15f95b4368dbfde
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-15 17:09:09 +0200

    1.7 functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -3,12 +3,23 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    String someone= "workld";
-    System.out.println("Hello, " + someone);
-    double l=8.0;
-    double s=l*l;
 
-    System.out.println("Quare with side  "+ l +" = " +s);
-
+    hello("world");
+    hello("user");
+    hello("Juls");
+    double l =5;
+    System.out.println("square with side " + l +" = " + area(l));
+    double a =4;
+    double b=6;
+    System.out.println("square with side " + a + "and" +  b +" = " + area(l));
+  }
+  public static void hello(String someone) {
+    System.out.println("Hello, " + someone + "!");
+  }
+  public static double area (double len){
+    return len*len;
+  }
+  public static double area (double a, double b){
+    return a*b;
   }
 }
