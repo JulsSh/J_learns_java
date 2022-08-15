2e2a5c726f1db63bdbad544a3ae310653ef5d210
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-15 16:38:04 +0200

    1.5 variables types

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    String someone= "workld";
+    System.out.println("Hello, " + someone);
+    double l=8.0;
+    double s=l*l;
+
+    System.out.println("Quare with side  "+ l +" = " +s);
 
   }
 }
