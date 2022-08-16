6d25823117a307f93069ff763fbf43fb3d9e1c94
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-16 11:56:30 +0200

    1.8 Classes and objects

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -7,17 +7,22 @@ public class HelloWorld {
     hello("world");
     hello("user");
     hello("Juls");
-    double l =5;
-    System.out.println("square with side " + l +" = " + area(l));
-    double a =4;
-    double b=6;
-    System.out.println("square with side " + a + "and" +  b +" = " + area(l));
+
+    Square s= new Square(5);
+    System.out.println("square with side " + s.l +" = " + area(s));
+
+    Rectangle r=new Rectangle(4,6);
+
+    System.out.println("square with side " + r.a + "and" +  r.b +" = " + area(r));
   }
   public static void hello(String someone) {
     System.out.println("Hello, " + someone + "!");
   }
-  public static double area (double len){
-    return len*len;
+  public static double area (Square s){
+    return s.l*s.l;
+  }
+  public static double area (Rectangle r) {
+    return r.a * r.b;
   }
   public static double area (double a, double b){
     return a*b;
