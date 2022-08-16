db4aaa4422e8d252540e7c28db4f5abd0ee017f3
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-16 12:11:32 +0200

    1.9 Methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -9,21 +9,17 @@ public class HelloWorld {
     hello("Juls");
 
     Square s= new Square(5);
-    System.out.println("square with side " + s.l +" = " + area(s));
+    System.out.println("square with side " + s.l +" = " + s.area());
 
     Rectangle r=new Rectangle(4,6);
 
-    System.out.println("square with side " + r.a + "and" +  r.b +" = " + area(r));
+    System.out.println("square with side " + r.a + "and" +  r.b +" = " + r.area());
   }
   public static void hello(String someone) {
     System.out.println("Hello, " + someone + "!");
   }
-  public static double area (Square s){
-    return s.l*s.l;
-  }
-  public static double area (Rectangle r) {
-    return r.a * r.b;
-  }
+
+
   public static double area (double a, double b){
     return a*b;
   }
