c38f555eb1373d74bcaab8751dae403e22d724f5
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-17 12:34:48 +0200

    misc

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDist.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDist.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 class PointDist {
 
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     Point p1 = new Point(-17, 8);
 
     Point p2 = new Point(-1, 0);
