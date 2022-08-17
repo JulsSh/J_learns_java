3dd9e4e571602ca1a899a60c19883d17cd55b72e
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-17 11:26:55 +0200

    homework #2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -24,14 +24,3 @@ class Point {
   }
 }
 
-class PointDist {
-
-  public static void main(String args[]) {
-    Point p1 = new Point(-17, 8);
-
-    Point p2 = new Point(-1, 0);
-
-    System.out.println("the distance between 2 points is " + p1.distance(p2));
-
-  }
-}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDist.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+class PointDist {
+
+  public static void main(String args[]) {
+    Point p1 = new Point(-17, 8);
+
+    Point p2 = new Point(-1, 0);
+
+    System.out.println("the distance between 2 points is " + p1.distance(p2));
+
+  }
+}
