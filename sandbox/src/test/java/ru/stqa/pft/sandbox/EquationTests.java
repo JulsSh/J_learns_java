068fefb9f007d10c5f0adebcdb1d4d88f7a9548f
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-29 12:05:56 +0200

    3.1 Condition if

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTests {
+  @Test
+  public void test0() {
+    Equation e= new Equation(1,1,1);
+    Assert.assertEquals(e.);
+
+  }
+}
