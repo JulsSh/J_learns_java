67331a8e309854ac82fe396611430708537f79ec
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-29 15:47:10 +0200

    3.3 starting in different browsers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,8 +15,18 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
   private StringBuffer verificationErrors = new StringBuffer();
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
+       if (browser==BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    } else if(browser==BrowserType.CHROME){
+      wd=new ChromeDriver();
+    } else if (browser==BrowserType.IE)
   wd = new FirefoxDriver();
   wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   wd.get("http://localhost/addressbook/group.php");
