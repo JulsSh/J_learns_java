3eabb780c7ba201a1034f56acc67e684dd5c42a7
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-08-31 21:04:53 +0200

    4.1 while+ for

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrime(){
+   Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+  @Test
+    public void testNonPrime(){
+      Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
+
+    }
+  @Test(enabled = false)
+  public void testPrimeLong(){
+    long n=Integer.MAX_VALUE;
+    assertTrue(Primes.isPrime(n));
+  }
+}
