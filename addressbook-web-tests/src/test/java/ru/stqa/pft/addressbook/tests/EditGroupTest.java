e6038682d42ba4a3d49eecfc17def20cbe49c8c9
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-09-01 16:35:23 +0200

    4.3 added groupCounter to all tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/EditGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/EditGroupTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -7,6 +8,7 @@ public class EditGroupTest extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    int before =app.getGroupHelper().getGroupCount();
     if (!app.getGroupHelper().IsThereAGroup()){
       app.getGroupHelper().createGroup (new GroupData("test1", null, null));
     }
@@ -15,6 +17,8 @@ public class EditGroupTest extends TestBase {
     app.getGroupHelper().fillGroupForm(new GroupData("jul", "jul2", "jul3"));
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().returnToHome();
+    int after =app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);
 
   }
 }
