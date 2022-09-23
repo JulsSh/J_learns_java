4e14ff3052df4bf39a34fc0df8cb8587b38ac5da
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-09-23 20:51:02 +0200

    6.3 jCommander library

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import org.checkerframework.framework.qual.DefaultQualifier;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -11,14 +14,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-  public static void main (String[] args) throws IOException {
-    int count=Integer.parseInt(args[0]);
-    File file =new File(args[1]);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
+  public  void main (String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander= new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch(ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+  private void run() throws IOException {
+
     List<GroupData> groups =generateGroups(count);
-    save(groups, file);
+    save(groups, new File(file));
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private  void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer =new FileWriter(file);
     for (GroupData group: groups){
