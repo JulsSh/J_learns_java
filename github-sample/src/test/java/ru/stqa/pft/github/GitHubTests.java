106fd6a2a5c3ee89b9588fe0649628f8a2c06d67
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-10-18 11:41:14 +0200

    9.6 GitHUb

--- github-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
@@ -4,13 +4,15 @@ import com.google.common.collect.ImmutableMap;
 import com.jcabi.github.*;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class GitHubTests {
   @Test
-  public void testCommits(){
-    Github github = new RtGithub(" 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");
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
    RepoCommits commits= github.repos().get(new Coordinates.Simple("JulsSh", "j_learns_java")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())){
-      System.out.println(commit);
+      System.out.println(new RepoCommit.Smart(commit).message());
 
     }
   }
