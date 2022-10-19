f598bcd02f92ed0d32179d4935cae152c72bcac7
JulsSh <96484632+JulsSh@users.noreply.github.com>
2022-10-19 13:09:35 +0200

    10.2 remote browser launch

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,10 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -36,14 +39,20 @@ private final Properties properties;
     String target=System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
+if ("".equals(properties.getProperty("selenium.server"))) {
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.EDGE)) {
-      wd = new EdgeDriver();
-    }
+  if (browser.equals(BrowserType.FIREFOX)) {
+    wd = new FirefoxDriver();
+  } else if (browser.equals(BrowserType.CHROME)) {
+    wd = new ChromeDriver();
+  } else if (browser.equals(BrowserType.EDGE)) {
+    wd = new EdgeDriver();
+  }
+}else {
+  DesiredCapabilities capabilities= new DesiredCapabilities();
+  capabilities.setBrowserName(browser);
+  wd=new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+}
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseURL"));
     groupHelper = new GroupHelper(wd);
